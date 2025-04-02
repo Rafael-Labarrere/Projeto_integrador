acfea55aaa6b7087ab1d234aaedc52f221cb6b37
Rafael-Labarrere <162750472+Rafael-Labarrere@users.noreply.github.com>
2025-04-02 10:21:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-import { createServer } from 'node:http'
+/*    import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-    response.write('Hello world')
+    const server = createServer((request, response) => {
+        response.write('Hello world!')
 
-    return response.end()
+        return response.end()
+    })
+
+    server.listen(4321)
+    */
+
+import {fastify} from "fastify"
+
+const server = fastify()
+
+server.get('/' , () => {
+    return 'Hello World!'}
+)
+
+server.get('/pt' , () => {
+    return 'Olá Mundo!'}
+)
+
+server.get('/fr' , () => {
+    return 'Bonjour le monde!'}
+)
+
+server.listen({
+    port: 4321,
 })
-
-server.listen(4321)
